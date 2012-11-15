16262bb50c81527361da7b2377d283ec155e3586
koydas <koydas@hotmail.com>
2012-11-15 00:53:46 -0500

    Random color of trucks

--- parkingArea.js
+++ parkingArea.js
@@ -18,17 +18,22 @@ robert_the_lifter.ParkingArea = function(game) {
       .setSize(game.truckParkingWidth, game.truckParkingHeight)
       .setPosition(game.truckParkingX, game.truckParkingY)
       .setFill(parkingTiles);
-    
-  var truckTile = new lime.fill.Frame('images/trucks.png', 0, 0, this.truckWidth, this.truckHeight);
+  
+  // each slot of the array is a truck of a different color
+  var truckTile = new Array();
+  truckTile[0] = new lime.fill.Frame('images/trucks.png', 0, 0, this.truckWidth, this.truckHeight);
+  truckTile[1] = new lime.fill.Frame('images/trucks.png', 60, 0, this.truckWidth, this.truckHeight);
+  truckTile[2] = new lime.fill.Frame('images/trucks.png', 120, 0, this.truckWidth, this.truckHeight);
+  truckTile[3] = new lime.fill.Frame('images/trucks.png', 185, 0, this.truckWidth, this.truckHeight);
   
   var nbTrucks = game.truckParkingWidth / game.tileWidth;
   for(var i = 0; i < nbTrucks; i ++) {
-    
+    var random_truck = Math.floor(Math.random()*4);
     var truck = new lime.Sprite()
       .setAnchorPoint(0, 0)
       .setSize(this.truckWidth, this.truckHeight)
       .setPosition(i*game.tileWidth, game.truckParkingY)
-      .setFill(truckTile);
+      .setFill(truckTile[random_truck]);
     
     this.appendChild(truck);
   }
